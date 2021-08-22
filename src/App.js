b5283de044b0d396a109c6430cb0125bd7e678b1
hajarNasr <hajar@weventure.global>
2021-08-22 21:38:14 +0200

    feat: create and style footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Hero from "./sections/hero/Hero";
 import WhoWeAre from "./sections/who_we_are";
 import Features from "./sections/features";
 import Testimonials from "./sections/testimonials";
+import Footer from "./sections/footer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <WhoWeAre />
       <Features />
       <Testimonials />
+      <Footer />
     </div>
   );
 }
