d5d260c0aa220da26ed33b7a86724dba797a9025
hajarNasr <hajar@weventure.global>
2021-08-22 16:34:43 +0200

    _: add images

---
+++ src/utils/resources.js
@@ -0,0 +1,17 @@
+const PATH = "/assets/images";
+export const LOGO = `${PATH}/logo.png`;
+export const HERO = `${PATH}/hero.png`;
+export const ARROW_RIGHT = `${PATH}/arrow-right.png`;
+export const ARROW_LEFT = `${PATH}/arrow-left.png`;
+export const WHITE_ARROW_RIGHT = `${PATH}/white-arrow-right.png`;
+export const WHO_WE_ARE = `${PATH}/storage-bank.png`;
+export const FEAT_1 = `${PATH}/feat1.png`;
+export const FEAT_2 = `${PATH}/feat2.png`;
+export const FEAT_3 = `${PATH}/feat3.png`;
+export const FEAT_4 = `${PATH}/feat4.png`;
+export const SHAPE_1 = `${PATH}/shape1.png`;
+export const SHAPE_2 = `${PATH}/shape2.png`;
+export const SHAPE_3 = `${PATH}/shape3.png`;
+export const PHOTO_1 = `${PATH}/photo1.png`;
+export const PHOTO_2 = `${PATH}/photo2.png`;
+export const PHOTO_3 = `${PATH}/photo3.png`;
