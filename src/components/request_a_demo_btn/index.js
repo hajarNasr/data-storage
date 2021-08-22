9d6f89deca55d93c2f1d3b71ab6164fd95a4ec26
hajarNasr <hajar@weventure.global>
2021-08-22 21:26:50 +0200

    refactor: give RequestADemo a className

--- src/components/request_a_demo_btn/index.js
+++ src/components/request_a_demo_btn/index.js
@@ -2,7 +2,7 @@ import { ARROW_RIGHT } from "../../utils/resources";
 import "./demo_btn.scss";
 
 const RequestDemoBtn = () => (
-  <button>
+  <button className="demo-btn">
     <span>Request Demo</span>
     <img src={ARROW_RIGHT} alt="" />
   </button>
